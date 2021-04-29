02a28a2f73e576c843d93c39dc2e0a0d91753f72
gang2Android <hujiangang888@gmail.com>
2021-04-29 21:02:51 +0800

    新增新媒体分类

--- server/model/news.go
+++ server/model/news.go
@@ -10,14 +10,14 @@ type News struct {
 	global.GVA_MODEL
 	User_id       int    `json:"user_id" form:"user_id" gorm:"column:user_id;comment:会员id;type:int;size:20;"`
 	User_name     string `json:"user_name" form:"user_name" gorm:"column:user_name;comment:会员昵称;type:varchar(20);size:20;"`
-	User_img      string `json:"user_img" form:"user_img" gorm:"column:user_img;comment:会员头像;type:varchar(50);size:50;"`
+	User_img      string `json:"user_img" form:"user_img" gorm:"column:user_img;comment:会员头像;type:varchar(100);size:100;"`
 	Title         string `json:"title" form:"title" gorm:"column:title;comment:标题;type:varchar(100);size:100;"`
 	Desc          string `json:"desc" form:"desc" gorm:"column:desc;comment:内容描述;type:varchar(255);size:255;"`
-	Img_main      string `json:"img_main" form:"img_main" gorm:"column:img_main;comment:主图/预览图;type:varchar(50);size:50;"`
+	Img_main      string `json:"img_main" form:"img_main" gorm:"column:img_main;comment:主图/预览图;type:varchar(255);size:255;"`
 	Cid           int    `json:"cid" form:"cid" gorm:"column:cid;comment:分类id;type:int;size:20;"`
 	Cname         string `json:"cname" form:"cname" gorm:"column:cname;comment:分类名称;type:varchar(20);size:20;"`
-	Imgs_url      string `json:"imgs_url" form:"imgs_url" gorm:"column:imgs_url;comment:图片地址,多个以逗号分隔(,);type:varchar(255);size:255;"`
-	Video_url     string `json:"video_url" form:"video_url" gorm:"column:video_url;comment:视频地址;type:varchar(255);size:255;"`
+	Imgs_url      string `json:"imgs_url" form:"imgs_url" gorm:"column:imgs_url;comment:图片地址,多个以逗号分隔(,);type:text;size:0;"`
+	Video_url     string `json:"video_url" form:"video_url" gorm:"column:video_url;comment:视频地址;type:text;size:0;"`
 	Comment_count int    `json:"comment_count" form:"comment_count" gorm:"column:comment_count;comment:评论数;type:int;size:100;"`
 	Like_count    int    `json:"like_count" form:"like_count" gorm:"column:like_count;comment:点赞数;type:int;size:100;"`
 	Collect_count int    `json:"collect_count" form:"collect_count" gorm:"column:collect_count;comment:收藏数;type:int;size:100;"`
